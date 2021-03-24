36c9ad66560e4576003193efaa00ab6b409d2c76
RickySandi <rickysandis@gmail.com>
2021-03-24 11:29:30 -0400

    refactorizando

--- README_refactoring.md
+++ README_refactoring.md
@@ -1,2 +1,17 @@
 # dirtyCodeExample
 Refactorización
+
+Línea 1: El nombre del método definido es **multiplicar_matrices** por lo que el método solo debería hacer eso, cumpliendo con el pricipio de Single Responsability. 
+
+Code Smell: 
+
+Refactoring: Abstraer las lecturas por teclado en otro método. 
+
+Línea 2: la variable muchos_argumentos = False nunca es utilizada. 
+
+Code Smell:
+
+Refactoring: Eliminar esa variable
+
+
+
--- multiplicar_matrices_by_Camacho_Encinas_Jhonny.py
+++ multiplicar_matrices_by_Camacho_Encinas_Jhonny.py
@@ -1,5 +1,10 @@
 def multiplicar_matrices(m1, m2):
-    muchos_argumentos = False
+    
+    ingresar_datos(m1, m2)
+    return [ [sum([m1[i][k]*[fila[j] for fila in m2][k] for k in range(len(m1[i]))]) for j in range(len(m2[0]))] for i in range(len(m1))]
+
+
+def ingresar_datos(m1,m2):
     x1, y1 = tuple([int(x) for x in input("ingrese dimensiones de la primera matriz de forma -> x y: ").split()])
     print('Ingrese fila por fila la primera matriz')
     for i in range(x1):
@@ -18,7 +23,7 @@ def multiplicar_matrices(m1, m2):
         if(len(m2[0]) > y2):
             print("Muchos argumentos en la fila")
             exit()
-    return [ [sum([m1[i][k]*[fila[j] for fila in m2][k] for k in range(len(m1[i]))]) for j in range(len(m2[0]))] for i in range(len(m1))]
+
             
     
 m1 = []
