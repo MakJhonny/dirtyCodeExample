79532910a05ed4b1ccf1c1a7a84c970873541b72
RickySandi <rickysandis@gmail.com>
2021-03-24 23:44:11 -0400

    Comparar dimensiones

--- README_refactoring.md
+++ README_refactoring.md
@@ -13,11 +13,19 @@ Code Smell:
 
 Refactoring: Eliminar esa variable
 
-Líneas 4-9 y 16-20: Existe código duplicado que puede ser parte de un método nuevoç 
+Líneas 4-9 y 16-20: Existe código duplicado que puede ser parte de un método nuevo.
 
 Code Smell: 
 
-Refactoring: Creación de un nuevo método llamado **leer_aegumentos()** que recibe x, y y la matriz m 
+Refactoring: Creación de un nuevo método llamado **leer_argumentos()** que recibe x, y y la matriz m 
+
+Líneas 12-14: Estas tres líneas hacen una comprueban que el número de columnas de la primera matriz sea igual al número de filas de la segunda matriz. El if de la línea 12 no es muy claro y todo el código mencionado puede ser parte de un método con un nombre descriptivo. 
+
+Code Smell:
+
+Refactoring: Creación de un nuevo método llamado **comparar_dimensiones()**
+
+Líneas 3 y 11: Es
 
 
 
--- multiplicar_matrices_by_Camacho_Encinas_Jhonny.py
+++ multiplicar_matrices_by_Camacho_Encinas_Jhonny.py
@@ -3,23 +3,27 @@ def multiplicar_matrices(m1, m2):
     ingresar_datos(m1, m2)
     return [ [sum([m1[i][k]*[fila[j] for fila in m2][k] for k in range(len(m1[i]))]) for j in range(len(m2[0]))] for i in range(len(m1))]
 
-
-def ingresar_datos(m1,m2):
-    x1, y1 = tuple([int(x) for x in input("ingrese dimensiones de la primera matriz de forma -> x y: ").split()])
-    leer_aegumentos(x1,y1,m1)
-    x2, y2 = tuple([int(x) for x in input("ingrese dimensiones de la segunda matriz de forma -> x y: ").split()])
-    if(y1 != x2):
-        print("Las dimensiones de las matrices no son compatibles para multiplicarse")
-        exit()
-    leer_aegumentos(x2,y2,m2)
-
-def leer_aegumentos(xi, yi, mi):
+def leer_argumentos(xi, yi, mi):
     print('Ingrese fila por fila la matriz')
     for i in range(xi):
         mi += [[int(x) for x in input().split()]]
         if(len(mi[0]) > yi):
             print("Muchos argumentos en la fila")
             exit()
+
+def comparar_dimensiones(y1, x2):
+    if(y1 != x2):
+        print("Las dimensiones de las matrices no son compatibles para multiplicarse")
+        exit()
+
+def ingresar_datos(m1,m2):
+    x1, y1 = tuple([int(x) for x in input("ingrese dimensiones de la primera matriz de forma -> x y: ").split()])
+    leer_argumentos(x1,y1,m1)
+    x2, y2 = tuple([int(x) for x in input("ingrese dimensiones de la segunda matriz de forma -> x y: ").split()])
+    comparar_dimensiones(y1, x2)
+    leer_argumentos(x2,y2,m2)
+
+
 
 
             
