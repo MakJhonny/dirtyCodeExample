f3b6f09823fe2f90ea032d0e4e7a5473b123cb9d
RickySandi <rickysandis@gmail.com>
2021-03-24 23:22:01 -0400

    leer_argumentos()

--- README_refactoring.md
+++ README_refactoring.md
@@ -5,13 +5,21 @@ Línea 1: El nombre del método definido es **multiplicar_matrices** por lo que 
 
 Code Smell: 
 
-Refactoring: Abstraer las lecturas por teclado en otro método. 
+Refactoring: Abstraer las lecturas por teclado en otro método. El metodo se llama ingresar_datos()
 
 Línea 2: la variable muchos_argumentos = False nunca es utilizada. 
 
 Code Smell:
 
 Refactoring: Eliminar esa variable
+
+Líneas 4-9 y 16-20: Existe código duplicado que puede ser parte de un método nuevoç 
+
+Code Smell: 
+
+Refactoring: Creación de un nuevo método llamado **leer_aegumentos()** que recibe x, y y la matriz m 
+
+
 
 
 
--- codigo_original.py
+++ codigo_original.py
@@ -1,27 +1,24 @@
 def multiplicar_matrices(m1, m2):
-    
-    ingresar_datos(m1, m2)
-    return [ [sum([m1[i][k]*[fila[j] for fila in m2][k] for k in range(len(m1[i]))]) for j in range(len(m2[0]))] for i in range(len(m1))]
-
-
-def ingresar_datos(m1,m2):
+    muchos_argumentos = False
     x1, y1 = tuple([int(x) for x in input("ingrese dimensiones de la primera matriz de forma -> x y: ").split()])
-    leer_aegumentos(x1,y1,m1)
+    print('Ingrese fila por fila la primera matriz')
+    for i in range(x1):
+        m1 += [[int(x) for x in input().split()]]
+        if(len(m1[0]) > y1):
+            print("Muchos argumentos en la fila")
+            exit()
+    
     x2, y2 = tuple([int(x) for x in input("ingrese dimensiones de la segunda matriz de forma -> x y: ").split()])
     if(y1 != x2):
         print("Las dimensiones de las matrices no son compatibles para multiplicarse")
         exit()
-    leer_aegumentos(x2,y2,m2)
-
-def leer_aegumentos(xi, yi, mi):
-    print('Ingrese fila por fila la matriz')
-    for i in range(xi):
-        mi += [[int(x) for x in input().split()]]
-        if(len(mi[0]) > yi):
+    print('Ingrese fila por fila la segunda matriz')
+    for i in range(x2):
+        m2 += [[int(x) for x in input().split()]]
+        if(len(m2[0]) > y2):
             print("Muchos argumentos en la fila")
             exit()
-
-
+    return [ [sum([m1[i][k]*[fila[j] for fila in m2][k] for k in range(len(m1[i]))]) for j in range(len(m2[0]))] for i in range(len(m1))]
             
     
 m1 = []
